5614012fbfe1a379186ff436f03c0e9d583f1183
Shuang Yuan <yuanshuang1996@126.com>
2022-03-12 16:26:54 +0800

    committed 2022-03-12

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,20 @@
 			<div class="title">Civilopedia</div>
 			<div class="header">
 				<div class="header-list">
-
+					<div><img alt="Concepts" title="Concepts" src='./assets/buttons/concepts.webp'></div>
 				</div>
 			</div>
 			<div class="content">
-
+				<div class="side-panel"></div>
+				<div class="main-content"></div>
 			</div>
 		</div>
 	</div>
 </template>
+
+<script>
+
+</script>
 
 <style>
 body {
@@ -22,9 +27,10 @@ body {
 	display: flex;
 	font-family: 'Penumbra HalfSerif Std';
 	justify-content: center;
+	padding: 40px;
 }
 .wrapper {
-	width: 950px;
+	width: 820px;
 	border: 59px solid;
 	border-image: url('./assets/core/controls_windowtitle.png') 165 fill;
 	border-image-repeat: stretch;
@@ -43,89 +49,9 @@ body {
 }
 .header {
 	background-image: url('./assets/core/composite_tabledge2.png');
+	background-repeat: round;
 	background-size: cover;
-	width: 1028px;
+	width: 900px;
 	height: 61px;
 }
 </style>
-<!-- <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style> -->
